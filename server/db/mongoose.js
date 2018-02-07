d2d36086ea18b2d2ea8ecd964a47212f509479e2
Vlad Strogov <techedelweis@gmail.com>
2018-02-06 21:20:34 -0500

    Setup separate test and development env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://vlad:Strogov53@ds225038.mlab.com:25038/todoapp' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://vlad:Strogov53@ds225038.mlab.com:25038/todoapp');
 module.exports={mongoose};
