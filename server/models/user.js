5430a26578522863208bfb8cc8e48589f61aa80f
Vlad Strogov <techedelweis@gmail.com>
2018-02-08 20:45:47 -0500

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -49,7 +49,26 @@ UserSchema.methods.generateAuthToken=function(){
   return user.save().then(()=>{
     return token;
   });
-}
+};
+
+UserSchema.statics.findByToken=function(token){
+  var User = this;
+  var decoded;
+
+  try{
+    decoded= jwt.verify(token,'123abc');
+  }catch(e){
+  // return new Promise((resolve,reject)=>{
+  // reject();
+     return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id':decoded._id,
+    'tokens.token':token,
+    'tokens.access':'auth'
+  });
+};
 
 var User = mongoose.model('Users',UserSchema);
 
