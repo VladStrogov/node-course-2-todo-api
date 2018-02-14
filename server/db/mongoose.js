95ac8ebf96dd6dac8599044672cf85f44d2f085c
Vlad Strogov <techedelweis@gmail.com>
2018-02-14 11:32:09 -0500

    Add tests for GET /users/me and POST /users

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
- mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+ mongoose.connect(process.env.MONGODB_URI /*|| 'mongodb://localhost:27017/TodoApp'*/);
 //mongoose.connect('mongodb://vlad:Strogov53@ds225038.mlab.com:25038/todoapp');
 module.exports={mongoose};
